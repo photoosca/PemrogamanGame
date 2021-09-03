fbee8c503c6a27a1c977b9da0f2afc51d4b4dffb
photoosca <88528506+photoosca@users.noreply.github.com>
2021-09-03 17:15:09 +0700

    Bukti Belajar Pert - 05
    pong2d selesaid

---
+++ Pong2D/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+public class PaddleController : MonoBehaviour
+{
+    public float batasAtas;
+    public float batasBawah;
+    public float kecepatan;
+    public string axis;
+    public string hor;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float gerak = Input.GetAxis(axis) * kecepatan * Time.deltaTime;
+        float nextPos = transform.position.y + gerak;
+        if (nextPos > batasAtas)
+        {
+            gerak = 0;
+        }
+        if (nextPos < batasBawah)
+        {
+            gerak = 0;
+        }
+        transform.Translate(0, gerak, 0);
+    }
+}
